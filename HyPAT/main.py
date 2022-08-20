13e50c0b032e8510326769c302913d074e74e377
GeorgeEvans0 <George.Evans@INL.gov>
2022-08-20 15:48:02 -0600

    Updating the repository
    Specifically, updating it by undoing the last commit.

--- HyPAT/main.py
+++ HyPAT/main.py
@@ -7,7 +7,7 @@
     compatible: macOS and Windows 10 """
 from source_code.application import Application
 
-# . This is the first of several test commits
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app = Application()
